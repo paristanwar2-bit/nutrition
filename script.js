4e426c9651246cf5b70013858a14cbec8c1f2a08
jagdish gupta <paristanwar2@gmail.com>
2025-11-06 13:25:53 +0530

    nn

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Mobile Navbar Toggle
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
+// 🌿 Navbar Toggle for Mobile
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
 
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
 
 
